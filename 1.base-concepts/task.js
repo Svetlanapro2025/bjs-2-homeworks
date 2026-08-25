726756acec4da7ae30562531840b100dc66231cf
Korn <basket.sv@yandex.ru>
2026-08-25 14:09:32 +0300

    Задание 1.base-concepts готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = Math.pow(b, 2) - 4 * a * c;
   
-  return arr;
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    let root = -b / (2 * a);
+    return [root];
+  } else {
+    let rootFirst = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let rootSecond = (-b - Math.sqrt(discriminant)) / (2 * a);
+    return [rootFirst, rootSecond];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / 100 / 12;
+  let loanAmount = amount - contribution;
+  let paymentMonth = loanAmount * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) -1 )));
+  let totalPayments = Math.round(paymentMonth * countMonths * 100) / 100;
+
+  return totalPayments;
 }
